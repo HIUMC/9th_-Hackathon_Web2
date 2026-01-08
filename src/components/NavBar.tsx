b3da5552372c8d467cc5b6dbb06d860551aca8e4
Seony777 <addiolsw1@gmail.com>
2026-01-08 12:14:54 +0900

    feat: NavBar 컴포넌트 구현

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import wallet from "../assets/material-symbols_wallet.svg";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="px-[48px] py-[20px] w-[1440px] border-b border-[#F4F6F8] bg-[#FFF]">
+      <div className="flex items-center gap-[12px]">
+        {/* 지갑 아이콘 */}
+        <img src={wallet} alt="wallet" className="w-[32px] h-[32px] shrink-0 aspect-square" />
 
-export default NavBar
+        {/* 로고 텍스트 */}
+        <span className="text-main font-sans text-[28px] font-semibold leading-[150%] tracking-[-0.56px]">
+          Pocket safe
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
