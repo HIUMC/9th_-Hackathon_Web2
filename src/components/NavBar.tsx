2e9d8219f669be1b575a36cd9c71c55e2c666769
Seony777 <addiolsw1@gmail.com>
2026-01-08 12:21:28 +0900

    feat: 로고 클릭 시 홈 이동 기능 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import wallet from "../assets/material-symbols_wallet.svg";
 
 const NavBar = () => {
   return (
-    <div className="px-[48px] py-[20px] w-[1440px] border-b border-[#F4F6F8] bg-[#FFF]">
-      <div className="flex items-center gap-[12px]">
+    <div className="px-[48px] py-[20px] w-[1440px] border-b border-white bg-white">
+      <Link to="/" className="flex items-center gap-[12px] cursor-pointer">
         {/* 지갑 아이콘 */}
         <img src={wallet} alt="wallet" className="w-[32px] h-[32px] shrink-0 aspect-square" />
 
@@ -11,7 +12,7 @@ const NavBar = () => {
         <span className="text-main font-sans text-[28px] font-semibold leading-[150%] tracking-[-0.56px]">
           Pocket safe
         </span>
-      </div>
+      </Link>
     </div>
   );
 };
